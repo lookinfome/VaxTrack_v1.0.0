78ffbc1e7dc6bb14ee812692ad01b86765453fb5
lookinfo <vivekkr8235@gmail.com>
2024-10-25 13:52:36 +0530

    added alert messages on successful login and registration

--- obj/Debug/net8.0/VaxTrack_v1.AssemblyInfo.cs
+++ obj/Debug/net8.0/VaxTrack_v1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VaxTrack_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d1cf43f0e38ab89514f66448fae69f6cc8c05e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db8114e44f45191e8e048254061343e9c1a0ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VaxTrack_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VaxTrack_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
